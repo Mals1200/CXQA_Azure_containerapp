a9be26df1c44fcce1a43c696682155d91bfab9c8
Mals1200 <168693176+Mals1200@users.noreply.github.com>
2025-03-12 05:20:53 +0300

    Update ask_func.py

--- ask_func.py
+++ ask_func.py
@@ -631,7 +631,7 @@ def agent_answer(user_question):
     
     # If user_question is empty or just whitespace
     if not user_question.strip():
-        return "Hello! I'm The CXQA AI Assistant. I'm here to help you. What would you like to know today?\n- To reset the conversation type 'restart chat'.\n- To generate Slides, Charts or Document, type 'export followed by your requirements."
+        return 
         
     # Check for repeated question in cache
     if user_question in tool_cache:
@@ -649,8 +649,8 @@ def agent_answer(user_question):
         else:
             result = (
                 "Hello! How may I assist you?\n"
-                "-To reset the conversation type 'restart chat'.\n"
-                "-To generate Slides, Charts or Document, type 'export followed by your requirements."
+                "- To reset the conversation type 'restart chat'.\n"
+                "- To generate Slides, Charts or Document, type 'export followed by your requirements."
             )
         tool_cache[user_question] = result
         return result
