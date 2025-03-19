fb010de5a6f3c8742f892f8696504e1a3f70f837
Mals1200 <168693176+Mals1200@users.noreply.github.com>
2025-03-19 04:49:10 +0300

    Create 17_ask_func(3 LLMs instead of one).py

---
+++ Backup/ask/17_ask_func(3 LLMs instead of one).py
@@ -0,0 +1,7 @@
+###########################################################################
+# Usiing 3 different LLM's for different tasks
+#   - gpt-4o: for all the agents with responses and calculations
+#   - gpt-4o-3: for Exports (changes in the Export_Agent.py file
+#   - gpt-4o-mini: for the Topic, Tool_2_Decision, and splitting functions
+###########################################################################
+
