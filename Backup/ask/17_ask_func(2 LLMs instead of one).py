f25b97fcb675237ae3e09c869ee9178e5f1b2eae
Mals1200 <168693176+Mals1200@users.noreply.github.com>
2025-03-19 14:45:42 +0300

    Update and rename 17_ask_func(3 LLMs instead of one).py to 17_ask_func(2 LLMs instead of one).py

--- Backup/ask/17_ask_func(2 LLMs instead of one).py
+++ Backup/ask/17_ask_func(2 LLMs instead of one).py
@@ -2,6 +2,6 @@
 # Usiing 3 different LLM's for different tasks
 #   - gpt-4o: for all the agents with responses and calculations
 #   - gpt-4o-3: for Exports (changes in the Export_Agent.py file
-#   - gpt-4o-mini: for the Topic, Tool_2_Decision, and splitting functions
+#   - gpt-4o: for the Topic, Tool_2_Decision, and splitting functions (might make it a dedicated deployment))
 ###########################################################################
 
